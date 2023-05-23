10df52a3deefcdefd079e72d19be1a3e0796eaad
Emmanuel Kasigazi <98601170+olimiemma@users.noreply.github.com>
2023-05-23 22:39:31 +0300

    Create python rate_things.py
    This code will first get the list of all the restaurants in the country specified by the user. It will then get the rating for each restaurant and calculate the average rating. Finally, it will print the average rating to the console.
    
    The code will prompt you to enter the name of the country. Once you have entered the name of the country, the code will print the average rating for restaurants in that country.

---
+++ python rate_things.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def get_ratings(country):
+  """Gets the ratings for all the things in a country.
+
+  Args:
+    country: The name of the country.
+
+  Returns:
+    A list of ratings.
+  """
+
+  url = "https://api.yelp.com/v3/businesses/search?term=restaurants&location={}".format(country)
+  response = requests.get(url, headers={"Authorization": "Bearer YOUR_API_KEY"})
+  if response.status_code == 200:
+    data = json.loads(response.content)
+    ratings = []
+    for business in data["businesses"]:
+      rating = business["rating"]
+      ratings.append(rating)
+    return ratings
+  else:
+    raise Exception("Error getting ratings: {}".format(response.status_code))
+
+def main():
+  """The main function."""
+
+  country = input("Enter the name of the country: ")
+  ratings = get_ratings(country)
+  print("The average rating for restaurants in {} is {}".format(country, sum(ratings) / len(ratings)))
+
+if __name__ == "__main__":
+  main()
+python rate_things.py
